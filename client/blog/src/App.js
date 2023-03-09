c6a9fdca71af8e17374378e62cd737fca0efe823
PraffulJha <54620668+PraffulJha@users.noreply.github.com>
2023-03-09 14:31:32 +0530

    initial commit

---
+++ client/blog/src/App.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import { Home } from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Single from './pages/Single';
+import Write from './pages/Write';
+import "./css/styles.scss"
+const Layout = () => {
+  return(
+  <>
+  <Navbar />
+  <Outlet />
+  <Footer />
+  </>
+  ) 
+}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/post.:id",
+        element: <Single />
+      },
+      {
+        path: "/write",
+        element: <Write />
+      },
+      {
+        path: "/",
+        element: <Home />
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+]);
+
+const App = () => {
+  return (
+    <div>
+      <div className='app'>
+        <div className='container'>
+          <RouterProvider router={router}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+
+export default App
---
+++ client/blog/src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='auth'>
+        <h1>Login</h1>
+        <form>
+            <input type="text" placeholder='username' />
+            <input type="password" placeholder='password' />
+            <button>Login</button>
+            <span> Don't have an account? <Link to ="/register"/></span>
+        </form>
+
+    </div>
+  )
+}
+
+export default Login
